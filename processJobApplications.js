66d84d1e5a7bf05a0c3895ca2989ca425fca4c97
Chris Burns <admin@chrisMacbook.local>
2025-01-28 11:42:18 -0500

    updating logic incase already applied

--- processJobApplications.js
+++ processJobApplications.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 const fillForm = require("./fillForm"); // Import the fillForm function
-const {jobApplication} = require("./data/config"); // Import the jobApplication data
+const { jobApplication } = require("./data/config"); // Import the jobApplication data
 
 /**
  * Processes each job ID to fill out and submit the Easy Apply form.
@@ -14,22 +14,30 @@ async function processJobApplications(page, jobIds) {
             const jobUrl = `https://www.linkedin.com/jobs/view/${jobId}`;
             await page.goto(jobUrl, { waitUntil: 'networkidle2' });
 
-            await page.waitForSelector('.jobs-apply-button--top-card .jobs-apply-button', { visible: true });
-            await page.click('.jobs-apply-button--top-card .jobs-apply-button');
-            await page.waitForSelector('.jobs-easy-apply-modal form', { visible: true });
+            // Check if the "Easy Apply" button exists
+            const applyButton = await page.$('.jobs-apply-button--top-card .jobs-apply-button', { visible: true, timeout: 3000 });
 
-            // Step 3: Fill out the form using the fillForm function
-            await fillForm(page, jobApplication);
+            if (applyButton) {
+                // Step 2: Click the "Apply" button
+                await applyButton.click();
+                await page.waitForSelector('.jobs-easy-apply-modal form', { visible: true });
 
-            // Step 4: Submit the form
-            console.log(`Successfully applied to job ${jobId}`);
+                // Step 3: Fill out the form using the fillForm function
+                await fillForm(page, jobApplication);
 
-            // Step 5: Wait for a brief moment to ensure the form is submitted before moving to the next
-            await page.waitForTimeout(3000); // Adjust as necessary
+                // Step 4: Submit the form
+                console.log(`Successfully applied to job ${jobId}`);
+
+                // Step 5: Wait for a brief moment to ensure the form is submitted before moving to the next
+                await page.waitForTimeout(3000); // Adjust as necessary
+            } else {
+                // If apply button doesn't exist, assume you applied. log already applied
+                console.log(`Already applied to job ${jobId}`);
+            }
+
         } catch (error) {
             console.error(`Error applying to job ${jobId}:`, error.message);
         }
-
     }
 }
 
